596ef6d5ac90928c556370786023f625ff4e46c1
rose <wittyrosegarcia@yahoo.com>
2020-06-04 17:25:01 -0400

    add new dice Roller

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ let numOfDiceRoll= document.querySelector("#numberOfDiceRoll");
 let numTotalDiceRoll= document.querySelector("#Total-Roll");
 let numRollButton= document.querySelector("#Roll-Button");
 let numResetButton= document.querySelector("#Reset-Button");
-let numShowAllResetButton= document.querySelector("#ShowAll-RollsButton");
+let numShowAllButton= document.querySelector("#ShowAll-RollsButton");
 let numListOfAllDieRoll= document.querySelector("#ListOfAll-DieRolls");
 
 let numberRolled;
@@ -14,13 +14,22 @@ numRollButton.addEventListener("click",function(){
    let newNumOfDiceRoll = Number(numOfDiceRoll.value)
    let counter = 0 
    let sum = 0
-   while (counter < newNumODiceRoll){
+   while (counter < newNumOfDiceRoll){
       numberRolled = Math.floor(Math.random()* 6) + 1
       dieRoll.push (numberRolled)
       counter++
       sum += numberRolled;
    }
-   numTotalDiceRoll.innerHTML = sum;
+   numTotalDiceRoll.innerHTML += sum;
 })
+
+numShowAllButton.addEventListener("click",function(){
+    for (let itemIndex = 0; itemIndex < dieRoll.length; itemIndex += 1){
+       let currentdieRoll = dieRoll[itemIndex]
+   
+
+    }
+})  
+
 
 
